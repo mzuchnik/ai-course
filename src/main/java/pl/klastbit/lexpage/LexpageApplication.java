7c6d0c7b20d5429149f3f76bf3c1dfa5de02ef09
mzuchnik <mateusz.zuchnik97@gmail.com>
2026-01-18 22:12:28 +0100

    Initial commit

---
+++ src/main/java/pl/klastbit/lexpage/LexpageApplication.java
@@ -0,0 +1,13 @@
+package pl.klastbit.lexpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LexpageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/klastbit/lexpage/LexpageApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klastbit.lexpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexpageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
